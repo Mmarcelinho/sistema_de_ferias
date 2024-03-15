39401ddb1b956b92d5a3b0042887c9eeacd940f8
Mmarcelinho <marcelorosario2001@gmail.com>
2024-03-15 19:38:16 -0300

    Adicionado implementação de DependencyInjection

---
+++ VacationSystem.IoC/AppDependencies/DependencyInjection.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VacationSystem.Infrastructure.Data;
+using VacationSystem.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore.Metadata;
+using VacationSystem.Infrastructure.Repositories;
+
+
+namespace VacationSystem.IoC.AppDependencies;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+      this IServiceCollection services,
+      IConfiguration configuration)
+    {
+        var SqlConnection = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<Context>(opt => opt.UseSqlServer(SqlConnection));
+
+        services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
+
+        services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
+
+        services.AddScoped<IAdminRepository, AdminRepository>();
+
+        services.AddScoped<IPedidoFeriasRepository, PedidoFeriasRepository>();
+
+        return services;
+    }
+}
