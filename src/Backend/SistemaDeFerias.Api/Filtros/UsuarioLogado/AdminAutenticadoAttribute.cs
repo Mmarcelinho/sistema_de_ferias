4ebd89981e422f17b20449859513a055405059dd
Mmarcelinho <marcelorosario2001@gmail.com>
2024-04-18 19:43:51 -0300

    Adicionado refatoração no filtro de autorização

--- src/Backend/SistemaDeFerias.Api/Filtros/UsuarioLogado/AdminAutenticadoAttribute.cs
+++ src/Backend/SistemaDeFerias.Api/Filtros/UsuarioLogado/AdminAutenticadoAttribute.cs
@@ -1,56 +1,13 @@
+using SistemaDeFerias.Infrastructure.AcessoRepositorio.Repositorio;
+
 namespace SistemaDeFerias.Api.Filtros.UsuarioLogado;
 
-public class AdminAutenticadoAttribute : AuthorizeAttribute, IAsyncAuthorizationFilter
+public class AdminAutenticadoAttribute : UsuarioAutenticadoAttribute<Domain.Entidades.Admin>
 {
     private readonly TokenController _tokenController;
 
-    private readonly IAdminReadOnlyRepositorio _repositorio;
+    private readonly UsuarioRepositorio<Domain.Entidades.Admin> _repositorio;
 
-    public AdminAutenticadoAttribute(TokenController tokenController, IAdminReadOnlyRepositorio repositorio)
-    {
-        _tokenController = tokenController;
-        _repositorio = repositorio;
-    }
-
-    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
-    {
-        try
-        {
-            var token = TokenNaRequisicao(context);
-            var emailAdmin = _tokenController.RecuperarEmail(token);
-
-            var admin = await _repositorio.RecuperarPorEmail(emailAdmin);
-
-            if (admin is null)
-                throw new SistemaDeFeriasException(string.Empty);
-        }
-        catch (SecurityTokenExpiredException)
-        {
-            TokenExpirado(context);
-        }
-        catch
-        {
-            UsuarioSemPermissao(context);
-        }
-    }
-    private static string TokenNaRequisicao(AuthorizationFilterContext context)
-    {
-        var authorizationHeader = "Authorization";
-        var authorization = context.HttpContext.Request.Headers[$"{authorizationHeader}"].ToString();
-
-        if (string.IsNullOrWhiteSpace(authorization))
-            throw new SistemaDeFeriasException(string.Empty);
-
-        return authorization["Bearer".Length..].Trim();
-    }
-
-    private static void TokenExpirado(AuthorizationFilterContext context)
-    {
-        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.TOKEN_EXPIRADO));
-    }
-
-    private static void UsuarioSemPermissao(AuthorizationFilterContext context)
-    {
-        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.USUARIO_SEM_PERMISSAO));
-    }
+    public AdminAutenticadoAttribute(TokenController tokenController, UsuarioRepositorio<Domain.Entidades.Admin> repositorio) : base(tokenController, repositorio)
+    { }
 }
--- src/Backend/SistemaDeFerias.Api/Filtros/UsuarioLogado/FuncionarioAutenticadoAttribute.cs
+++ src/Backend/SistemaDeFerias.Api/Filtros/UsuarioLogado/FuncionarioAutenticadoAttribute.cs
@@ -1,56 +1,13 @@
+using SistemaDeFerias.Infrastructure.AcessoRepositorio.Repositorio;
+
 namespace SistemaDeFerias.Api.Filtros.UsuarioLogado;
 
-public class FuncionarioAutenticadoAttribute : AuthorizeAttribute, IAsyncAuthorizationFilter
+public class FuncionarioAutenticadoAttribute : UsuarioAutenticadoAttribute<Domain.Entidades.Funcionario>
 {
     private readonly TokenController _tokenController;
 
-    private readonly IFuncionarioReadOnlyRepositorio _repositorio;
+    private readonly UsuarioRepositorio<Domain.Entidades.Funcionario> _repositorio;
 
-    public FuncionarioAutenticadoAttribute(TokenController tokenController, IFuncionarioReadOnlyRepositorio repositorio)
-    {
-        _tokenController = tokenController;
-        _repositorio = repositorio;
-    }
-
-    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
-    {
-        try
-        {
-            var token = TokenNaRequisicao(context);
-            var emailFuncionario = _tokenController.RecuperarEmail(token);
-
-            var funcionario = await _repositorio.RecuperarPorEmail(emailFuncionario);
-
-            if (funcionario is null)
-                throw new SistemaDeFeriasException(string.Empty);
-        }
-        catch (SecurityTokenExpiredException)
-        {
-            TokenExpirado(context);
-        }
-        catch
-        {
-            UsuarioSemPermissao(context);
-        }
-    }
-    private static string TokenNaRequisicao(AuthorizationFilterContext context)
-    {
-        var authorizationHeader = "Authorization";
-        var authorization = context.HttpContext.Request.Headers[$"{authorizationHeader}"].ToString();
-
-        if (string.IsNullOrWhiteSpace(authorization))
-            throw new SistemaDeFeriasException(string.Empty);
-
-        return authorization["Bearer".Length..].Trim();
-    }
-
-    private static void TokenExpirado(AuthorizationFilterContext context)
-    {
-        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.TOKEN_EXPIRADO));
-    }
-
-    private static void UsuarioSemPermissao(AuthorizationFilterContext context)
-    {
-        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.USUARIO_SEM_PERMISSAO));
-    }
+    public FuncionarioAutenticadoAttribute(TokenController tokenController, UsuarioRepositorio<Domain.Entidades.Funcionario> repositorio) : base(tokenController, repositorio)
+    { }
 }
