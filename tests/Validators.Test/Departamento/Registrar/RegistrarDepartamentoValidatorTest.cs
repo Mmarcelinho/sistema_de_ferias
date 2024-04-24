edd95f94238504cb7bac1b8275776cceafa13461
Mmarcelinho <marcelorosario2001@gmail.com>
2024-04-23 23:55:45 -0300

    Adicionado testes do validator de Departamento

---
+++ tests/Validators.Test/Departamento/Registrar/RegistrarDepartamentoValidatorTest.cs
@@ -0,0 +1,52 @@
+namespace Validators.Test.Departamento.Registrar;
+
+    public class RegistrarDepartamentoValidatorTest
+    {
+        [Fact]
+        public void Validar_Sucesso()
+        {
+            long departamentoId = 1;
+
+            var validator = new RegistrarDepartamentoValidator();
+
+            var requisicao = RequisicaoDepartamentoBuilder.Construir(departamentoId);
+
+            var resultado = validator.Validate(requisicao);
+
+            resultado.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validar_Erro_Nome_Vazio()
+        {
+            long departamentoId = 1;
+
+            var validator = new RegistrarDepartamentoValidator();
+
+            var requisicao = RequisicaoDepartamentoBuilder.Construir(departamentoId);
+            var requisicaoSemNome = requisicao with { Nome = string.Empty };
+
+            var resultado = validator.Validate(requisicaoSemNome);
+
+            resultado.IsValid.Should().BeFalse();
+
+            resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NOME_DO_DEPARTAMENTO_EMBRANCO));
+        }
+
+        [Fact]
+        public void Validar_Erro_Setor_Invalido()
+        {
+            long departamentoId = 1;
+
+            var validator = new RegistrarDepartamentoValidator();
+
+            var requisicao = RequisicaoDepartamentoBuilder.Construir(departamentoId);
+            var requisicaoSemSetor = requisicao with { SetorId = 0 };
+
+            var resultado = validator.Validate(requisicaoSemSetor);
+
+            resultado.IsValid.Should().BeFalse();
+
+            resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.SETOR_INVALIDO));
+        }
+    }
---
+++ tests/Validators.Test/Usings.cs
@@ -0,0 +1,14 @@
+global using FluentAssertions;
+
+global using SistemaDeFerias.Exceptions;
+
+global using Utilitario.Testes.Requisicoes.Setor;
+global using SistemaDeFerias.Application.UseCases.Setor.Registrar;
+global using SistemaDeFerias.Application.UseCases.Setor.Atualizar;
+
+global using Utilitario.Testes.Requisicoes.Departamento;
+global using SistemaDeFerias.Application.UseCases.Departamento.Registrar;
+global using SistemaDeFerias.Application.UseCases.Departamento.Atualizar;
+
+
+
