89b4b387e52e7e12f7262bfd8c4045162f7c588c
Mmarcelinho <marcelorosario2001@gmail.com>
2024-04-24 00:35:42 -0300

    Adicionado testes do validator de Usuario

---
+++ tests/Utilitario.Testes/Requisicoes/Usuario/RequisicaoRegistrarUsuarioBuilder.cs
@@ -0,0 +1,14 @@
+namespace Utilitario.Testes.Requisicoes.Usuario;
+
+public class RequisicaoRegistrarUsuarioBuilder
+{
+    public static RequisicaoRegistrarUsuarioJson Construir(int tamanhoSenha = 10)
+    {
+        return new Faker<RequisicaoRegistrarAdminJson>()
+            .RuleFor(c => c.Nome, f => f.Person.FullName)
+            .RuleFor(c => c.Email, f => f.Internet.Email())
+            .RuleFor(c => c.Senha, f => f.Internet.Password(tamanhoSenha))
+            .RuleFor(c => c.Telefone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+    }
+
+}
